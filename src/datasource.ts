3e39ffcff5a9e69f964dfbe5e94f3febb8e5027f
Jules Vautier <jules.vautier@sewan.fr>
2022-04-15 01:18:03 +0200

    v1

--- src/datasource.ts
+++ src/datasource.ts
@@ -3,12 +3,13 @@ import {
   DataQueryResponse,
   DataSourceApi,
   DataSourceInstanceSettings,
-  MutableDataFrame,
   FieldType,
+  MutableDataFrame,
 } from '@grafana/data';
-import { getBackendSrv } from '@grafana/runtime';
+import {getBackendSrv} from '@grafana/runtime';
 
-import { MyQuery, MyDataSourceOptions } from './types';
+import {defaultQuery, MyDataSourceOptions, MyQuery} from './types';
+import defaults from 'lodash/defaults';
 
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
@@ -16,29 +17,43 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   }
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
-    const promises = options.targets.map((query) =>
-      this.doRequest(query).then((response) => {
-        const frame = new MutableDataFrame({
-          refId: query.refId,
-          fields: [
-            { name: 'Time', type: FieldType.time },
-            { name: 'Value', type: FieldType.number },
-          ],
-        });
+    const {range} = options;
+    const from = range!.from.valueOf();
+    const to = range!.to.valueOf();
 
-        response.data.forEach((point: any) => {
-          frame.appendRow([point.tenant_id, point.tenant_id]);
-        });
+    const data = options.targets.map((target) => {
+      const query = defaults(target, defaultQuery);
 
-        return frame;
-      })
-    );
+      const frame = new MutableDataFrame({
+        refId: query.refId,
+        fields: [
+          {name: 'time', type: FieldType.time},
+          {name: 'value', type: FieldType.number},
+        ],
+      });
 
-    return Promise.all(promises).then((data) => ({ data }));
+      // duration of the time range, in milliseconds.
+      const duration = to - from;
+
+      // step determines how close in time (ms) the points will be to each other.
+      const step = duration / 1000;
+
+      for (let t = 0; t < duration; t += step) {
+        frame.add({time: from + t, value: Math.sin((2 * Math.PI * t) / duration)});
+      }
+
+      return frame;
+    });
+
+    return {data};
   }
 
   async doRequest(query: MyQuery) {
-    const result = await getBackendSrv().get('http://127.0.0.1:7280/api/v1/hdfs-logs/search?query=1');
+    const result = await getBackendSrv().datasourceRequest({
+      method: 'GET',
+      url: 'http://127.0.0.1:7280/api/v1/hdfs-logs/search?query=1',
+      headers: {'Access-Control-Allow-Origin': '*', 'Referrer-Policy': 'no-referrer'},
+    });
 
     return result;
   }
